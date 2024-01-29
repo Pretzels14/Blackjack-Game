af4713998c1892c7152bd4748ba5c7c6e5eb81f7
Pretzels14 <connorp54@gmail.com>
2024-01-28 22:12:40 -0600

    Added our card values to the enum class for CardRank, and created constructor as well as getter for the value of the card

--- CardRank.java
+++ CardRank.java
@@ -1,3 +1,13 @@
 enum CardRank {
+    TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), KING(10), QUEEN(10), ACE(11);
+    private int value;
+
+    CardRank(int value) {
+        this.value = value;
     
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
