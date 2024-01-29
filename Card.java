e9ab9155e4c4c3c086f3c399867e5fa24270ca91
Pretzels14 <connorp54@gmail.com>
2024-01-28 20:07:22 -0600

    created constructors and override function for Card class

--- Card.java
+++ Card.java
@@ -1,3 +1,18 @@
-public class Card {
+class Card {
+    private CardRank rank;
+    private CardSuit suit;
+
+    public Card(CardRank rank, CardSuit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
     
+    public int getCardVal() {
+        return rank.getCardVal();
+    }
+
+    @Override
+    public String toString() {
+        return rank + " " + suit;
+    }
 }
