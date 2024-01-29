37f987323a2fce777332b6c3f43d002caf14c075
Pretzels14 <connorp54@gmail.com>
2024-01-28 22:23:12 -0600

    Added Comments and began to write some code for my functions

--- Card.java
+++ Card.java
@@ -1,16 +1,24 @@
 class Card {
+    // Declare our objects of CardRank and CardSuit
     private CardRank rank;
     private CardSuit suit;
 
+    /**
+     * 
+     * @param rank the rank of the card selected from the deck
+     * @param suit the suit of the card selected from the deck
+     */
     public Card(CardRank rank, CardSuit suit) {
         this.rank = rank;
         this.suit = suit;
     }
     
+    // return the CardRank's value as an int
     public int getCardVal() {
-        return rank.getCardVal();
+        return rank.getValue();
     }
 
+    // Convert our rank and suit into a string line
     @Override
     public String toString() {
         return rank + " " + suit;
